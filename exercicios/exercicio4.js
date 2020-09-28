e058a8da036a1c818f7571cc17b2054c64b76265
botelholarissa <conta.larissa@gmail.com>
2020-09-28 16:33:38 -0300

    exercícios 1 a 4 realizados

--- exercicios/exercicio4.js
+++ exercicios/exercicio4.js
@@ -20,3 +20,51 @@ const listaDeMercado = [
     comprado: false
   }
 ]
+
+//console.log(listaDeMercado[0]["comprado"])
+
+
+
+
+/* const listaFiltrada = listaDeMercado.filter((i) =>  (listaDeMercado[i].comprado) == true)
+console.log(listaFiltrada) */
+
+/* const listaFiltrada = listaDeMercado.filter(function(i){listaDeMercado[i]["comprado"] == true})
+console.log(listaFiltrada)  */
+
+let novaLista = []
+
+for(i = 0; i < listaDeMercado.length; i++){
+  const lista = listaDeMercado[i].comprado
+  if (lista === true){
+    //console.log(listaDeMercado[i])
+    novaLista.push(listaDeMercado[i])
+  } 
+}
+
+console.log(novaLista)
+
+
+
+/* function filterByComprado(obj){
+  if ('comprado' in obj === true){
+    return t
+  } else {
+    return 'não'
+  }
+}
+
+console.log(filterByComprado()) */
+
+/* const arrById = listaDeMercado.filter(filterByComprado)
+
+console.log(arrById) */
+/* 
+const listaFiltrada = listaDeMercado.map(function (item) {
+  const lista = listaDeMercado[item]["comprado"]
+  if (lista === true) {
+    return lista
+  }
+})
+
+console.log(listaFiltrada) */
