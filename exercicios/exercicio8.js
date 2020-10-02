1fd596621dc1188e5191f63b7649c916c405cbca
botelholarissa <conta.larissa@gmail.com>
2020-10-02 20:56:29 -0300

    resolução ex 6, 7, 8 e 9

--- exercicios/exercicio8.js
+++ exercicios/exercicio8.js
@@ -1,1 +1,22 @@
 const nomes = ['Anna', 'Andressa', 'Luciana', 'Bruna', 'Amanda'];
+
+
+/* function filterItems(query) {
+    return nomes.filter(function(el) {
+        return el.indexOf(query) > -1;
+    })
+  }
+  
+  console.log(filterItems('A')); */
+
+  // Filtre uma lista de nomes e retorne somente os que começam com a letra 'A'
+
+/* const nomesInitialA = nomes.filter(nome => nome.indexOf('A') > -1)
+
+console.log(nomesInitialA) */
+
+
+const teste = nomes.indexOf('A') > -1
+console.log(teste)
+
+console.log(nomes)
--- exercicios/exercicio9.js
+++ exercicios/exercicio9.js
@@ -2,6 +2,24 @@ const confeito = ['>(O)<'];
 const pessoa = {
   nome: 'Carlos',
   comer(confeito) {
-    console.log(`${this.nome} comeu o confeito: ${praSuavariavel}`);
+    console.log(`${this.nome} comeu o confeito: ${confeitoReplace}`);
   }
 }
+
+
+//Uma pessoa é representada pelo objeto `pessoa`. Essa pessoa tem a habilidade de comer. A string `>(O)<` representa uma bala. Faça com que essa pessoa consiga comer essa bala, mas pra isso, você vai precisar tirá-la da embalagem.
+
+const confeitoReplace = confeito[0].replace(">(", "").replace(")<", "")
+
+pessoa.comer(confeitoReplace)
+
+
+/* const confeitoAberto = confeito.filter(index =>{
+  console.log(index)
+  index !== ">"
+  
+})  */
+
+//console.log(confeitoAberto)
+
+//|| index !== "<")
