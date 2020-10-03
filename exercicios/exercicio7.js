d36f1ba557bf87fdcf7d1c28037eefe35dc1fd29
edilenelopes <lopes_edi@yahoo.com.br>
2020-10-03 18:31:03 -0300

    Alterei o 7 e 8 prontos

--- exercicios/exercicio7.js
+++ exercicios/exercicio7.js
@@ -1,11 +1,15 @@
-const nomes = ['Pedro', 'Aurora', 'Anne', 'Rafa', 'Helena', 'Arthur','Murilo', 'Alice'];
+//Some somente os valores ímpares de uma lista
 
-//Filtre uma lista de nomes e retorne somente os que começam com a letra 'A'
+const valores = [2, 34, 56, 3, 9, 6, 55, 106]
 
-//função com parâmetros, o indexof retorna uma substring (no caso A)
-//que por padrão é zero, por isso o -1
-const filterNames = (query) => {
-    return nomes.filter(el => el.indexOf(query) > -1)
-}
+//função que calcula se é ímpar e guarda numa lista
+const Impares = valores.filter(item => item % 2 !== 0)
 
-console.log(filterNames('A'))
+console.log(Impares) 
+
+//função usa o método reduce que tem acumulador e item atual da função
+//acumulador e item atual do return
+//somando os valores ímpares da lista impares
+const valoresSomados = Impares.reduce((acumulador, itemAtual) => acumulador + itemAtual)
+
+console.log('O valor dos ímpares é: ', valoresSomados) 
